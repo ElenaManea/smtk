1b812d3342b9f0df9125d9b1b04562b3ae021034
flori <flory.manea88@gmail.com>
2016-11-15 11:06:22 +0200

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='smtk',
+      version='1.0',
+      description="SMTK installable via pip",
+      author="Elena Manea",
+      url="https://github.com/ElenaManea/smtk.git",
+      license="MIT",
+      packages=['smtk'],
+      zip_safe=False
+)
